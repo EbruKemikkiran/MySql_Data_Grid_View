03d5d7d80f029db6173f9f43b10c0bdf1e68698b
Ebru Kemikkiran <104608342+EbruKemikkiran@users.noreply.github.com>
2022-09-15 14:39:55 +0100

    Add files via upload

---
+++ MySql_Data_Grid_View/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace MySql_Data_Grid_View
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvUsers = new System.Windows.Forms.DataGridView();
+            this.btnActiveUsers = new System.Windows.Forms.Button();
+            this.btnPassiveUsers = new System.Windows.Forms.Button();
+            this.lblActiveUsers = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvUsers
+            // 
+            this.dgvUsers.AllowUserToAddRows = false;
+            this.dgvUsers.AllowUserToDeleteRows = false;
+            this.dgvUsers.AllowUserToOrderColumns = true;
+            this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsers.Location = new System.Drawing.Point(12, 84);
+            this.dgvUsers.Name = "dgvUsers";
+            this.dgvUsers.ReadOnly = true;
+            this.dgvUsers.RowHeadersWidth = 51;
+            this.dgvUsers.RowTemplate.Height = 24;
+            this.dgvUsers.Size = new System.Drawing.Size(589, 343);
+            this.dgvUsers.TabIndex = 0;
+            // 
+            // btnActiveUsers
+            // 
+            this.btnActiveUsers.Location = new System.Drawing.Point(28, 38);
+            this.btnActiveUsers.Name = "btnActiveUsers";
+            this.btnActiveUsers.Size = new System.Drawing.Size(143, 23);
+            this.btnActiveUsers.TabIndex = 1;
+            this.btnActiveUsers.Text = "Active Users";
+            this.btnActiveUsers.UseVisualStyleBackColor = true;
+            this.btnActiveUsers.Click += new System.EventHandler(this.btnActiveUsers_Click);
+            // 
+            // btnPassiveUsers
+            // 
+            this.btnPassiveUsers.Location = new System.Drawing.Point(447, 38);
+            this.btnPassiveUsers.Name = "btnPassiveUsers";
+            this.btnPassiveUsers.Size = new System.Drawing.Size(144, 23);
+            this.btnPassiveUsers.TabIndex = 1;
+            this.btnPassiveUsers.Text = "Passive Users";
+            this.btnPassiveUsers.UseVisualStyleBackColor = true;
+            this.btnPassiveUsers.Click += new System.EventHandler(this.btnPassiveUsers_Click);
+            // 
+            // lblActiveUsers
+            // 
+            this.lblActiveUsers.AutoSize = true;
+            this.lblActiveUsers.Location = new System.Drawing.Point(248, 38);
+            this.lblActiveUsers.Name = "lblActiveUsers";
+            this.lblActiveUsers.Size = new System.Drawing.Size(93, 16);
+            this.lblActiveUsers.TabIndex = 2;
+            this.lblActiveUsers.Text = "Active 0 Users";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblActiveUsers);
+            this.Controls.Add(this.btnPassiveUsers);
+            this.Controls.Add(this.btnActiveUsers);
+            this.Controls.Add(this.dgvUsers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvUsers;
+        private System.Windows.Forms.Button btnActiveUsers;
+        private System.Windows.Forms.Button btnPassiveUsers;
+        private System.Windows.Forms.Label lblActiveUsers;
+    }
+}
+
---
+++ MySql_Data_Grid_View/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MySql_Data_Grid_View
+{
+    public partial class Form1 : Form
+    {
+        string dbconstr = "SERVER=localhost; DATABASE=sales; UID=root; PWD=12345";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ActiveUsersTotal()
+
+
+        {
+            using (var connection = new MySqlConnection(dbconstr))
+            {
+                using (var cmd = new MySqlCommand("SELECET COUNT(city) AS TOTAL FROM sales WHERE city='london' ", connection))
+                {
+                    try
+                    {
+                        cmd.Connection.Open();
+                        MySqlDataReader dt = cmd.ExecuteReader();
+                        if (dt.Read())
+                        {
+                            int activeuserstoplam = Convert.ToInt32(dt["TOTAL"].ToString());
+                            if (activeuserstoplam < 1)
+                            {
+                                lblActiveUsers.Text = "Not Found Active Users";
+                            }
+                            else
+                            {
+                                lblActiveUsers.Text = $"TOTAL{activeuserstoplam} Active Users Found";
+                            }
+                        }
+
+                    }
+                    catch (Exception Error)
+                    {
+
+                        MessageBox.Show("There is an error:\n" +Error.Message);
+                    }
+
+
+                }
+
+            }
+        }
+
+        public void PassiveUsersTotal()
+
+
+        {
+            using (var connection = new MySqlConnection(dbconstr))
+            {
+                using (var cmd = new MySqlCommand("SELECET COUNT(city) AS TOTAL FROM sales WHERE city='manchester' ", connection))
+                {
+                    try
+                    {
+                        cmd.Connection.Open();
+                        MySqlDataReader dt = cmd.ExecuteReader();
+                        if (dt.Read())
+                        {
+                            int activeuserstoplam = Convert.ToInt32(dt["TOTAL"].ToString());
+                            if (activeuserstoplam < 1)
+                            {
+                                lblActiveUsers.Text = "Not Found Passive Users";
+                            }
+                            else
+                            {
+                                lblActiveUsers.Text = $"TOTAL{activeuserstoplam} Passive Users Found";
+                            }
+                        }
+
+                    }
+                    catch (Exception Error)
+                    {
+
+                        MessageBox.Show("There is an error:\n" + Error.Message);
+                    }
+
+
+                }
+
+            }
+        }
+
+
+        private void btnActiveUsers_Click(object sender, EventArgs e)
+        {
+            #region active users pulled up           
+            using (var connect = new MySqlConnection(dbconstr))
+            {
+                using (var adap = new MySqlDataAdapter("SELECT * FROM sales WHERE city='london'", connect))
+                {
+                    try
+                    {
+                        connect.Open();
+                        DataTable dt = new DataTable();
+                        adap.Fill(dt);
+                        dgvUsers.DataSource = dt;
+                        dgvUsers.Columns[0].HeaderText = "CUSTOMER ID";
+                        dgvUsers.Columns[1].HeaderText = "INVOICE";
+                        dgvUsers.Columns[2].HeaderText = "BRANCH";
+                        dgvUsers.Columns[3].HeaderText = "CITY";
+                        dgvUsers.Columns[4].HeaderText = "CUSTOMER";
+                        dgvUsers.Columns[5].HeaderText = "GENDER";
+                        dgvUsers.Columns[6].HeaderText = "PRODUCT";
+                        connect.Close();
+
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+
+                    }
+                }
+            }
+            #endregion
+            
+        }
+
+
+        private void btnPassiveUsers_Click(object sender, EventArgs e)
+        {
+            #region Passive users pulled up
+            using (var connect = new MySqlConnection(dbconstr))
+            {
+                using (var adap = new MySqlDataAdapter("SELECT * FROM sales WHERE city ='manchester'", connect))
+                {
+                    try
+                    {
+                        connect.Open();
+                        DataTable dt = new DataTable();
+                        adap.Fill(dt);
+                        dgvUsers.DataSource = dt;
+                        dgvUsers.Columns[0].HeaderText = "CUSTOMER ID";
+                        dgvUsers.Columns[0].Visible = false;
+                        dgvUsers.Columns[1].HeaderText = "INVOICE";
+                        dgvUsers.Columns[2].HeaderText = "BRANCH";
+                        dgvUsers.Columns[3].HeaderText = "CITY";
+                        dgvUsers.Columns[4].HeaderText = "CUSTOMER";
+                        dgvUsers.Columns[5].HeaderText = "GENDER";
+                        dgvUsers.Columns[6].HeaderText = "PRODUCT";
+                        connect.Close();
+
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+            #endregion
+        }
+    }
+}
+
+
+
+
